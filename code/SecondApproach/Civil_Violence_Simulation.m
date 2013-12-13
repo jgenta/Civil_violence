8406c4d878727f0312e5636b26729275038722e9
phineasng <anguyen@student.ethz.ch>
2013-12-13 20:33:02 +0100

    Update Civil_Violence_Simulation.m

--- code/SecondApproach/Civil_Violence_Simulation.m
+++ code/SecondApproach/Civil_Violence_Simulation.m
@@ -3,23 +3,26 @@
 clear all
 close all
 clc
+% Vectors to store results
 MaxClusterSizeLocal=zeros(10,200,5,5,5);
 MaxClusterSizeGlobal=zeros(10,200,5,5,5);
 ActivesLocal=zeros(10,200,5,5,5);
 ActivesGlobal=zeros(10,200,5,5,5);
+
+% Set Values of the parameters
 Legitimacy = 0.1:0.2:0.9;
 Threshold = -0.4:0.2:0.4;
 Cops = [0.01,0.02,0.05,0.08,0.09];
+
+% Start Simulations : 10 per parameters' combination
 for z=1:5
     for j=1:5
         for k=1:5
             for i = 1:10
-                tic
                 disp(strcat('Simulation Number ',num2str(i),': Local'))
                 [MaxClusterSizeLocal(i,:,j,k,z),ActivesLocal(i,:,j,k,z)]=civil_violence_main(20,0.2,Cops(z),Legitimacy(j),Threshold(k),10,200,0);
                 disp(strcat('Simulation Number ',num2str(i),': Global'))
                 [MaxClusterSizeGlobal(i,:,j,k,z),ActivesGlobal(i,:,j,k,z)]=civil_violence_main(20,0.2,Cops(z),Legitimacy(j),Threshold(k),10,200,1);
-                toc
             end
         end
     end
